e68a3702882a5b03247cadf1b35b776b5b17430f
clone4everyone <karanrawat9149@gmail.com>
2025-12-21 15:08:34 +0530

    uploaded live server url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:6000',
+        target: 'https://ms-publication-backend.onrender.com',
         changeOrigin: true
       }
     }
